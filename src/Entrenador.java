5ad441fff5d77b4c4469227b87fa85b4df14e813
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-03 09:19:31 +0100

    Agregado Atributos privados, constructor y métodos getters en clase Entrenador

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -11,4 +11,32 @@
  * @version 1.0
  */
 public class Entrenador {
+
+    private int id;
+    private String nombre;
+    private String especialidad;
+    private int añosExperiencia;
+
+    public Entrenador(int id, String nombre, String especialidad, int añosExperiencia) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.añosExperiencia = añosExperiencia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public int getAñosExperiencia() {
+        return añosExperiencia;
+    }
 }
